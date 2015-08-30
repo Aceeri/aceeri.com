31b6f6915c2245154b02182b4bf5cc9fba546921
Aceeri <conmcclusk@gmail.com>
2015-08-30 03:49:08 -0700

    package.json, ayylmao.html

--- index.js
+++ index.js
@@ -18,15 +18,9 @@ console.log('Project Directory: ' + __dirname);
 
 app.use(compression());
 app.use(serveStatic(__dirname + '/resources', {'index': ['/pages/template.html', '/pages/template.htm']}));
-/*app.use('/img', function(req, res) {
-	var html = '<!DOCTYPE html><html><header><title>Aceeri Image Viewer</title></header><body><img src="' + __dirname + "/resources/img/" + req.url + '" /></body></html>';
-
-	res.writeHead(200, {
-		'Content-Type': 'text/html',
-		'Content-Length': html.length,
-	});
-	res.end(html);
-});*/
+app.use('/t/', function(req, res) {
+	
+});
 
 // start listening on port 8000
 app.listen(8000);
