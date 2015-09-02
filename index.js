e7dbb2c6ba42f84f968525aeaff536d4d79ea7cc
unknown <conmcclusk@gmail.com>
2015-09-01 17:41:57 -0700

    fixes, redirecting, subdomains (CNAMEs), serving static html without extension, etc.

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var express = require('express');
+//var connect = require('connect');
 var compression = require('compression');
 var serveStatic = require('serve-static');
 var https = require('https');
 var http = require('http');
+var vhost = require('vhost');
+var fs = require('fs');
 var app = express();
 
-var port = 80;
+var host = fs.readFileSync("../host.txt");
+var port = (host == "localhost") ? 8000 : 80;
 
 process.on('uncaughtException', UncaughtExceptionHandler);
 
@@ -16,14 +20,41 @@ function UncaughtExceptionHandler(err) {
     setInterval(function() { }, 1000);
 }
 
-console.log('Project Directory: ' + __dirname);
-console.log('Port: ' + port);
+console.log("Dir: " + __dirname);
+console.log("Host: " + host);
+console.log("Port: " + port);
 
-function template(req, res) { res.sendFile(__dirname + "/resources/pages/template.html"); }
+function template(req, res) {
+	console.log("Getting template with " + req.originalUrl);
+	res.sendFile(__dirname + "/pages/template.html");
+}
+
+function subdomain(url) {
+	var pattern = new RegExp('(.*.?)' + host);
+	var match = url.match(pattern);
+
+	return match == undefined ? "" : match[1];
+}
+
+app.use(vhost(host, function(req, res) {
+	if (subdomain(req.headers.host) == "") {
+		console.log("redirect to www.host: " + req.protocol + '://www.' + host + req.originalUrl);
+		res.redirect(301, req.protocol + '://www.' + host + ":" + port + req.originalUrl);
+	}
+}));
+
+app.route(vhost("www." + host, function(req, res) {
+	console.log("using correct subdomain");
+}));
 
 app.use(compression());
 app.use('/resources', serveStatic(__dirname + '/resources'));
-app.use(serveStatic(__dirname + '/misc', {'index': ['/pages/template.html', '/pages/template.htm']}));
-app.get('/', template);
+app.use('/', serveStatic(__dirname + '/pages', {'extensions': ['html', 'htm']}));
+app.get('/*', template);
+
+// 404 page
+app.use(function(req, res) {
+	res.redirect('/pages/404.html');
+});
 
 app.listen(port);
