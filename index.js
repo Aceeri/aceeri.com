f4ea519a2210f2f069ccbffad8fedc06fe29ab8f
Aceeri <conmcclusk@gmail.com>
2015-09-02 22:18:13 -0700

    404 and paging changes

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 var express = require('express');
 //var connect = require('connect');
 var compression = require('compression');
-var serveStatic = require('serve-static');
+var serve_static = require('serve-static');
 var https = require('https');
 var http = require('http');
 var vhost = require('vhost');
 var fs = require('fs');
 var app = express();
 
+var output = true;
 var host = fs.readFileSync("../host.txt");
 var port = (host == "localhost") ? 8000 : 80;
 
-var template = fs.readFileSync(__dirname + "/pages/template.html"); // gets template for server session (reduces load)
+var template = fs.readFileSync(__dirname + "/pages/templated/template.html"); // gets template for server session (reduces load)
 
 process.on('uncaughtException', UncaughtExceptionHandler);
 
@@ -26,58 +27,69 @@ console.log("Host: " + host);
 console.log("Port: " + port);
 console.log("Template: " + (template != undefined));
 
-function templated_page(request, response) {
-	console.log("Getting template with " + request.originalUrl);
-	var page = template.toString();
+function get_page(url) {
 	var content;
-
 	try {
-		content = fs.readFileSync(__dirname + "/pages/templated/" + request.originalUrl);
-	} catch (err) { response.redirect('/404.html'); };
-	console.log(page);
+		content = fs.readFileSync(__dirname + "/pages/templated/" + url);
+	} catch (err) {
+		console.log("404");
+		content = fs.readFileSync(__dirname + "/pages/404.html");
+	}
 
-	//console.log(request.protocol + "://www." + host + ":" + port + "/resources/");
-	//page = page.replace(new RegExp("/?resources/"), request.protocol + "://www." + host + ":" + port + "/resources/");
+	return content;
+}
 
-	page = page.replace(new RegExp(/<div id="content">/), "<div id=\"content\">test");
+function templated_page(req, res) {
+	var page = template.toString();
+	var content = get_page(req.originalUrl);
 
+	page = page.replace(new RegExp(/<div id="content">/), "<div id=\"content\">" + content);
 
-	response.writeHead(200, {
-		'Content-Length': template.length,
+	res.writeHead(200, {
+		'Content-Length': page.length,
 		'Content-Type': 'text/html' });
-	response.write(page);
-	response.end();
-	//res.sendFile(__dirname + "/pages/template.html");
+	res.write(page);
+	res.end();
 }
 
 // returns subdomain
-function subdomain(url) {
+function get_subdomain(url) {
 	var pattern = new RegExp('(.*.?)' + host);
 	var match = url.match(pattern);
 
-	return match == undefined ? "" : match[1];
+	return match == undefined ? "" : match[1].slice(0, match[1].length - 1);
+}
+
+function not_found(req, res) {
+
 }
 
 app.use(vhost(host, function(req, res) {
 	if (subdomain(req.headers.host) == "") {
-		console.log("redirect to www.host: " + req.protocol + '://www.' + host + req.originalUrl);
+		console.log("redirect to www.host: " + req.protocol + '://www.' + host + ":" + port + req.originalUrl);
 		res.redirect(301, req.protocol + '://www.' + host + ":" + port + req.originalUrl);
 	}
 }));
 
-/*app.route(vhost("www." + host, function(req, res) {
-	console.log("using correct subdomain");
-}));*/
+app.use('/r/', serve_static(__dirname + "/resources/"));
+app.use('/m/', serve_static(__dirname + "/pages/misc/"));
+app.get('/*', function(req, res) {
+	console.log("request: " + req.headers.host + req.originalUrl);
 
-app.use(compression());
-app.use('/resources', serveStatic(__dirname + '/resources'));
-app.use('/', serveStatic(__dirname + '/pages', {'extensions': ['html', 'htm']}));
-//app.get('/404', function(req, res) { res.send('/pages/404.html'); });
-app.use('/*', templated_page);
+	var subdomain = get_subdomain(req.headers.host);
+	if (subdomain === "www") {
+		console.log("template domain");
 
-// 404 page
-app.use(function(req, res) {
-	res.redirect('/404.html');
+		templated_page(req, res);
+	} else if (subdomain === "chat") {
+		console.log("chat domain");
+
+		// 404 until created
+		not_found(req, res);
+	}
+
+	// no subdomains found
+	not_found(req, res);
 });
 
 app.listen(port);
